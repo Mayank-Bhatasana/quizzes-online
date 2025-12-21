92a4cadfa4e567f63fd72c2e0b5bb37e6d913e8f
Mayank-Bhatasana <bhatasanamayank06@gmail.com>
2025-12-21 17:53:54 +0530

    changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,23 @@
-import { resolve } from 'path'
-import { defineConfig } from 'vite'
-
+import { resolve } from "path";
+import { defineConfig } from "vite";
 export default defineConfig({
-  root: 'src', // Tells Vite that index.html is inside src
+  root: "src", // Tells Vite that index.html is inside src
   build: {
-    outDir: '../dist', // Builds to a folder outside of src
+    // @ts-ignore
+    envDir: "../", // <--- FIX 1: Tells Vite to look for .env in the parent folder
+    publicDir: "../public", // <--- FIX 2: Tells Vite where the public assets are
+    outDir: "../dist", // Builds to a folder outside of src
     emptyOutDir: true,
     rollupOptions: {
       input: {
         // Maps the URL path to the actual file
-        main: resolve(__dirname, 'src/index.html'),
-        auth: resolve(__dirname, 'src/auth/index.html'),
-        dashboard: resolve(__dirname, 'src/dashboard/index.html'),
-        difficulty: resolve(__dirname, 'src/difficulty/index.html'),
-        quiz: resolve(__dirname, 'src/quiz/index.html'),
-        results: resolve(__dirname, 'src/results/index.html'),
+        main: resolve(__dirname, "src/index.html"),
+        auth: resolve(__dirname, "src/auth/index.html"),
+        dashboard: resolve(__dirname, "src/dashboard/index.html"),
+        difficulty: resolve(__dirname, "src/difficulty/index.html"),
+        quiz: resolve(__dirname, "src/quiz/index.html"),
+        results: resolve(__dirname, "src/results/index.html"),
       },
     },
   },
-})
+});
